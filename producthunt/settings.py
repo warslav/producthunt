4960d61cb71376da815585e57c5aba02b0aef4a1
Slav win <Sl-aj@yandex.ua>
2019-06-15 15:34:05 +0300

    Change postgres to sqllite

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'producthunt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'producthuntdb',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
